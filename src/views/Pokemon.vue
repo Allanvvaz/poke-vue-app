c5dfae717508d41c3bdb1131a3fec998c3cc73dc
Allan Victor <allan.vvaz@gmail.com>
2025-04-04 19:13:34 -0300

    change

--- src/views/Pokemon.vue
+++ src/views/Pokemon.vue
@@ -1,6 +1,4 @@
-/* eslint-disable vue/no-use-v-if-with-v-for */
-/* eslint-disable no-unused-vars */
-/* eslint-disable vue/no-multiple-template-root */
+
 <template>
   <div class="container mt-4">
     <PokemonDetails />
